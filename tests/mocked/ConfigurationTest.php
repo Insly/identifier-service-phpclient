d133a1c97f2ceb0d72f05515e5ae0e97856efe16
Marcin Tracz <marcin.tracz@blumilk.pl>
2021-12-14 11:58:48 +0100

    Improve setting host value; adjust tests; remove unused exception

--- tests/mocked/ConfigurationTest.php
+++ tests/mocked/ConfigurationTest.php
@@ -3,20 +3,19 @@
 declare(strict_types=1);
 
 use Insly\Identifier\Client\Config;
-use Insly\Identifier\Client\Exceptions\InvalidHostFormatException;
 use PHPUnit\Framework\TestCase;
 
 class ConfigurationTest extends TestCase
 {
     public function testHostWithoutSlashAtTheEnd(): void
     {
-        $this->expectException(InvalidHostFormatException::class);
-        new Config("https://example.com/api/v1", "tenant");
+        $config = new Config("https://example.com/api/v1", "tenant");
+        $this->assertStringEndsWith("/", $config->getHost());
     }
 
     public function testHostWithSlashAtTheEnd(): void
     {
         $config = new Config("https://example.com/api/v1/", "tenant");
-        $this->assertNotEmpty($config);
+        $this->assertStringEndsWith("/", $config->getHost());
     }
 }
