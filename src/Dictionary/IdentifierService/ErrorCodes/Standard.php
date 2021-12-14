a6118b9cb7de63390dcf91c5742342e08db7db89
Marcin Tracz <marcin.tracz@blumilk.pl>
2021-12-14 15:12:25 +0100

    Add IDS codes dictionaries

---
+++ src/Dictionary/IdentifierService/ErrorCodes/Standard.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Insly\Identifier\Client\Dictionary\IdentifierService\ErrorCodes;
+
+abstract class Standard
+{
+    public const IDS_10001 = "IDS10001";
+    public const IDS_10002 = "IDS10002";
+    public const IDS_10003 = "IDS10003";
+    public const IDS_10004 = "IDS10004";
+    public const IDS_10005 = "IDS10005";
+    public const IDS_10006 = "IDS10006";
+    public const IDS_10007 = "IDS10007";
+    public const IDS_10008 = "IDS10008";
+    public const IDS_10009 = "IDS10009";
+    public const IDS_10010 = "IDS10010";
+    public const IDS_10011 = "IDS10011";
+    public const IDS_10012 = "IDS10012";
+    public const IDS_10013 = "IDS10013";
+    public const IDS_10014 = "IDS10014";
+    public const IDS_10015 = "IDS10015";
+    public const IDS_10016 = "IDS10016";
+    public const IDS_10017 = "IDS10017";
+    public const IDS_10018 = "IDS10018";
+    public const IDS_10019 = "IDS10019";
+    public const IDS_10020 = "IDS10020";
+    public const IDS_10021 = "IDS10021";
+    public const IDS_10022 = "IDS10022";
+    public const IDS_10023 = "IDS10023";
+    public const IDS_10024 = "IDS10024";
+    public const IDS_10025 = "IDS10025";
+    public const IDS_10026 = "IDS10026";
+    public const IDS_10027 = "IDS10027";
+    public const IDS_10028 = "IDS10028";
+    public const IDS_10029 = "IDS10029";
+    public const IDS_99999 = "IDS99999";
+}
