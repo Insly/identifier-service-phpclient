f303917f78d6e0ff6d9c078dd6e39c1d6d8d4bea
Marcin Tracz <marcin.tracz@blumilk.pl>
2021-12-15 08:55:05 +0100

    improve configuration tests

--- tests/mocked/ConfigurationTest.php
+++ tests/mocked/ConfigurationTest.php
@@ -10,12 +10,12 @@ class ConfigurationTest extends TestCase
     public function testHostWithoutSlashAtTheEnd(): void
     {
         $config = new Config("https://example.com/api/v1", "tenant");
-        $this->assertStringEndsWith("/", $config->getHost());
+        $this->assertStringEndsWith("v1/", $config->getHost());
     }
 
     public function testHostWithSlashAtTheEnd(): void
     {
         $config = new Config("https://example.com/api/v1/", "tenant");
-        $this->assertStringEndsWith("/", $config->getHost());
+        $this->assertStringEndsWith("v1/", $config->getHost());
     }
 }
