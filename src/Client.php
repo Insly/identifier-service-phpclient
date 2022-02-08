7cc289e92fa0b2ed7b39c05039fc9d606d261649
Aksel Meola <aksel@meola.eu>
2022-02-08 10:59:07 +0200

    Allow login with runtime credentials with additional meta info
    Login credentials are pre-configured in Config, which does not provide
    modification during runtime.
    
    https://app.beta.insly.training/api/v1/identifier/doc/swagger/index.html
    POST /login/{tenant_tag}
    
    INSLYPROD-3037

--- src/Client.php
+++ src/Client.php
@@ -37,17 +37,31 @@ class Client
     }
 
     /**
-     * @throws ClientExceptionInterface
      * @throws ExtractResponseException
      * @throws IdentifierServiceClientException
      * @throws JsonException
      */
     public function login(): ResponseInterface
     {
+        return $this->loginWithPassword(
+            $this->config->getUsername(),
+            $this->config->getPassword(),
+            $this->config->getLoginMeta(),
+        );
+    }
+
+    /**
+     * @throws ExtractResponseException
+     * @throws IdentifierServiceClientException
+     * @throws JsonException
+     */
+    public function loginWithPassword(string $username, string $password, array $meta): ResponseInterface
+    {
         $endpoint = $this->config->getHost() . "login/" . $this->config->getTenant();
         $credentials = [
-            "username" => $this->config->getUsername(),
-            "password" => $this->config->getPassword(),
+            "username" => $username,
+            "password" => $password,
+            "meta_data" => $meta,
         ];
 
         $request = new Request(RequestMethod::METHOD_POST, $endpoint, [], json_encode($credentials, flags: JSON_THROW_ON_ERROR));
